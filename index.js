0d7df159391210b28a0631f6d4650b7ade54a804
Andrew Shanks <andrew.shanks@contrastsecurity.com>
2019-10-05 22:10:58 +0100

    update frlom var to let, allow package.json to be pushed for CI builds

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 "use strict";
 
-var Alexa = require("alexa-sdk");
-var SKILL_NAME = "Confidence Builder";
-var APP_ID = "";
+let Alexa = require("alexa-sdk");
+let SKILL_NAME = "Confidence Builder";
+let APP_ID = "";
 
-var COMPLIMENT_LIST = [
+let COMPLIMENT_LIST = [
     "Your looking great this morning!",
     "Today is going to be a good day",
     "get up and have some chocolate",
@@ -12,13 +12,13 @@ var COMPLIMENT_LIST = [
 ];
 
 exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
+    let alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
-var handlers = {
+let handlers = {
     'LaunchRequest': function(){
         this.emit('GetCompliment');
     },
@@ -26,16 +26,16 @@ var handlers = {
         this.emit('GetCompliment');
     },
     'GetCompliment': function () {
-        var complimentIndex = Math.floor(Math.random() * COMPLIMENT_LIST.length);
-        var randomComplimdent = COMPLIMENT_LIST[complimentIndex];
+        let complimentIndex = Math.floor(Math.random() * COMPLIMENT_LIST.length);
+        let randomComplimdent = COMPLIMENT_LIST[complimentIndex];
 
         //output
-        var speechOutput = "Your compliment " + randomComplimdent;
+        let speechOutput = "Your compliment " + randomComplimdent;
         this.emit(":tellWithCard", speechOutput, SKILL_NAME, randomComplimdent);
     },
     'AMAZON.Helpintent': function() {
-        var speechOutput = "You can say giver me a compliment, or, you casn say exit... What can I help you with?";
-        var reprompt = "What can I help you with";
+        let speechOutput = "You can say giver me a compliment, or, you casn say exit... What can I help you with?";
+        let reprompt = "What can I help you with";
         this.emit(":ask", speechOutput, reprompt);
     },
     'AMAZON.Cancelintent': function() {
